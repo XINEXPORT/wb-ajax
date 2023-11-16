f6986eb03e61e746065cc3a5a7b29c223f8b7f66
Christine Hoang <christinehoangt@gmail.com>
2023-11-16 15:47:27 -0600

    weather forecast function complete

--- ajax-exercise.js
+++ ajax-exercise.js
@@ -18,15 +18,20 @@ async function showDogPhoto(evt) {
 
 document.querySelector('#get-dog-image').addEventListener('click', showDogPhoto);
 
+
+
 // PART 2: Show Weather
 
-function showWeather(evt) {
+async function  showWeather(evt) {
   const zipcode = document.querySelector('#zipcode-field').value;
-
-  // TODO: request weather with that URL and show the forecast in #weather-info
+  const response = await axios.get(`/weather.txt?q=${zipcode}`);
+  let weather = response.data;
+  
+  document.querySelector('#weather-info').innerHTML = weather;
 }
 
 document.querySelector('#weather-button').addEventListener('click', showWeather);
+
 
 // PART 3: Order Cookies
 
--- app.js
+++ app.js
@@ -22,11 +22,22 @@ app.get('/', (req, res) => {
   res.sendFile('index.html', { root: '.' });
 });
 
-app.get('/weather.txt', (req, res) => {
-  const zipcode = req.query.zipcode;
   // TODO: Get the weather for this zipcode and return the forecast if available.
   // If not, return the default forecast.
+
+app.get('/weather.txt', (req, res) => {
+const zipcode = req.query["q"];
+console.log(zipcode);
+const weather = WEATHER[zipcode]
+console.log(weather);
+ if (weather){
+  res.send(weather.forecast);
+ }
+else{
+  res.send(DEFAULT_FORECAST);
+}
 })
+
 
 app.post('/order-cookies.json', (req, res) => {
   const cookieType = req.body.cookieType;
